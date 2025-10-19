b8fd17c087fdfcc929c376174f18baedb3297e10
jorge lizan candela <jlizancandela@gmail.com>
2025-10-19 09:29:50 +0000

    Try fixing this error: `Console Error: Store does not have a valid reduc

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,25 +1,17 @@
 'use client';
-import { AppStore, makeStore } from '@/lib/store';
-import { ReactNode, useRef } from 'react';
-import { Provider } from 'react-redux';
+import { ReactNode } from 'react';
 import { FirebaseProvider, initializeFirebase } from '.';
 
 export function FirebaseClientProvider({ children }: { children: ReactNode }) {
-  const storeRef = useRef<AppStore>();
-  if (!storeRef.current) {
-    storeRef.current = makeStore();
-  }
   const { firebaseApp, firestore, auth } = initializeFirebase();
 
   return (
-    <Provider store={storeRef.current}>
-      <FirebaseProvider
-        firebaseApp={firebaseApp}
-        auth={auth}
-        firestore={firestore}
-      >
-        {children}
-      </FirebaseProvider>
-    </Provider>
+    <FirebaseProvider
+      firebaseApp={firebaseApp}
+      auth={auth}
+      firestore={firestore}
+    >
+      {children}
+    </FirebaseProvider>
   );
 }
