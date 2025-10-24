c641af1de55f0288b5d79463e7aeda679f8ae1af
jorge lizan candela <jlizancandela@gmail.com>
2025-10-24 08:12:05 +0000

    FirebaseError: Firebase Storage: No default bucket found. Did you set th

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -15,14 +15,16 @@
  *   apiKey: string,
  *   authDomain: string,
  *   measurementId: string,
- *   messagingSenderId: string
+ *   messagingSenderId: string,
+ *   storageBucket: string
  * }}
  */
 export const firebaseConfig = {
-  "projectId": "studio-4856973197-91469",
-  "appId": "1:517159420673:web:43d9982017c1bb4140d100",
-  "apiKey": "AIzaSyB6BiBi_KjH3_NGRAqrjWF-3NQdnillSk4",
-  "authDomain": "studio-4856973197-91469.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "517159420673"
+    "projectId": "studio-4856973197-91469",
+    "appId": "1:517159420673:web:43d9982017c1bb4140d100",
+    "apiKey": "AIzaSyB6BiBi_KjH3_NGRAqrjWF-3NQdnillSk4",
+    "authDomain": "studio-4856973197-91469.firebaseapp.com",
+    "measurementId": "",
+    "messagingSenderId": "517159420673",
+    "storageBucket": "studio-4856973197-91469.appspot.com"
 };
